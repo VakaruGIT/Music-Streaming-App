483256de236ebd5855604ef7d29d7955ff36a552
Andrei-Flavius Văcaru <andreivacaru2002@yahoo.com>
2024-06-19 19:56:39 +0200

    Made changes

--- music_streaming_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from music_streaming_app.models import *
-
-# Register your models here.
-class MusicAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(User, MusicAdmin)
